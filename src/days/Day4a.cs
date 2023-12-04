fee2183c8ba25796af434d33ffe0e55b3a685abd
Valter <valter.gadin@consultant.volvo.com>
2023-12-04 06:42:57 +0000

    Finished Day4 part 1

--- src/days/Day4a.cs
+++ src/days/Day4a.cs
@@ -14,7 +14,14 @@ namespace AdventOfCode.src.days
 
         public string Name 
         { 
-            get { return "Day4 - Part a"; }
+            get { return "Day4 - Part 1"; }
+        }
+
+        private static int NumbersToPoints(int num)
+        {
+            return num <= 0 ? 0 : Enumerable
+                .Repeat(2, num - 1)
+                .Aggregate(1, (a, b) => a * b);
         }
 
         public void Solve()
@@ -22,12 +29,43 @@ namespace AdventOfCode.src.days
             try
             {
                 string workingDirectory = Environment.CurrentDirectory;
-                string[] lines = File.ReadAllLines(workingDirectory + "/input/day3.txt");
+                Solution = File.ReadLines(workingDirectory + "/input/day4.txt")
+                    .Select(s => 
+                        {
+                            var slaks = s.Split(':');
+                            var numberSets = slaks[1].Split('|');
+                            return (WinningNumbers: numberSets[0], YouNumbers: numberSets[1]);
+                        }) 
+                    .Select(ss =>
+                    {
+                        var winningNumbersList =  FindNumber().Matches(ss.WinningNumbers)
+                                    .Cast<Match>()
+                                    .Select(m => int.Parse(m.Groups[1].Value))
+                                    .ToList();
+
+                        var youNumbersList =  FindNumber().Matches(ss.YouNumbers)
+                                    .Cast<Match>()
+                                    .Select(m => int.Parse(m.Groups[1].Value))
+                                    .ToList();
+
+                        return (WinningNumbers: winningNumbersList, YouNumbers: youNumbersList);
+                    })
+                    .Sum(ls =>
+                    {
+                        var matchingNumbers = ls.WinningNumbers.Count(wn => ls.YouNumbers.Contains(wn));
+                        var points = NumbersToPoints(matchingNumbers);
+                        Console.WriteLine(matchingNumbers +"->"+ points);
+                        return points;
+                    })
+                    .ToString();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An error occurred: " + ex.Message);
             }
         }
+
+        [GeneratedRegex(@"(\d+)")]
+        private static partial Regex FindNumber();
     }
 }
